179e41b1a75622e4e22f1607cc7dc44fa7a4828a
Masa Orel <orel.masha89@gmail.com>
2022-08-09 15:07:35 +0200

    added a few styles and removed border

--- script.js
+++ script.js
@@ -1,37 +1,37 @@
 // sketchpad
-let sketchPad = document.getElementById("sketch-pad");
+let sketchPad = document.getElementById('sketch-pad')
 // new grid btn
-let newGrid = document.querySelector(".btn-newGrid");
+let newGrid = document.querySelector('.btn-newGrid')
 
 // 16 by 16 grid
-for(let i = 1; i <= 256; i++) {
-    let box = document.createElement("div");
-    box.className = "boxes"
-    box.style.border = "1px solid blue";
-    sketchPad.appendChild(box)
-    
+for (let i = 1; i <= 256; i++) {
+  let box = document.createElement('div')
+  box.className = 'boxes'
+  sketchPad.appendChild(box)
 }
 // add color on mouseover
-document.body.addEventListener("mouseover", function(e) {
-    if(e.target.className == "boxes") {
-        e.target.style.backgroundColor = "orange"
-    }
+document.body.addEventListener('mouseover', function (e) {
+  if (e.target.className == 'boxes') {
+    e.target.style.backgroundColor = 'orange'
+  }
 })
 // add button
-newGrid.addEventListener("click", changeTheGrid)
-//make new grid 
+newGrid.addEventListener('click', changeTheGrid)
+//make new grid
 function changeTheGrid() {
-    sketchPad.innerHTML = "";
-    let userInput = prompt("enter a number of squares per side that is less than 100", "0")
-    let sideBoxes = parseInt(userInput)
-    sketchPad.style.gridTemplateColumns = `repeat(${sideBoxes}, 1fr)`
-    sketchPad.style.gridTemplateRows = `repeat(${sideBoxes}, 1fr)`
-    let grid = sideBoxes * sideBoxes;
+  sketchPad.innerHTML = ''
+  let userInput = prompt(
+    'enter a number of squares per side that is less than 100',
+    '0',
+  )
+  let sideBoxes = parseInt(userInput)
+  sketchPad.style.gridTemplateColumns = `repeat(${sideBoxes}, 1fr)`
+  sketchPad.style.gridTemplateRows = `repeat(${sideBoxes}, 1fr)`
+  let grid = sideBoxes * sideBoxes
 
-    for(let i = 1; i <= grid; i++) {
-        let box = document.createElement("div");
-        box.className = "boxes"
-        box.style.border = "1px solid blue";
-        sketchPad.appendChild(box)   
-    }
+  for (let i = 1; i <= grid; i++) {
+    let box = document.createElement('div')
+    box.className = 'boxes'
+    sketchPad.appendChild(box)
+  }
 }
