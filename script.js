d7f0ac406b5845dc3c927219316e192be0382b78
Masa Orel <orel.masha89@gmail.com>
2022-08-09 10:53:21 +0200

    mouse over with background color

--- script.js
+++ script.js
@@ -1,8 +1,16 @@
 let sketchPad = document.getElementById("sketch-pad")
 
+// 16 by 16 grid
 for(let i = 1; i <= 256; i++) {
     let box = document.createElement("div");
-    box.style.border = "1px solid blue";
+   box.style.border = "1px solid blue";
     sketchPad.appendChild(box);
     
+    
+    box.addEventListener("mouseover", function() {
+    box.style.backgroundColor = "orange"
+})
 }
+
+
+
