a04f5162c2789ba5cc5af2da1f56a158afc7747f
Masa Orel <orel.masha89@gmail.com>
2022-08-09 14:33:29 +0200

    added mouseover effect and a function that changes grid

--- script.js
+++ script.js
@@ -1,16 +1,37 @@
-let sketchPad = document.getElementById("sketch-pad")
+// sketchpad
+let sketchPad = document.getElementById("sketch-pad");
+// new grid btn
+let newGrid = document.querySelector(".btn-newGrid");
 
 // 16 by 16 grid
 for(let i = 1; i <= 256; i++) {
     let box = document.createElement("div");
-   box.style.border = "1px solid blue";
-    sketchPad.appendChild(box);
+    box.className = "boxes"
+    box.style.border = "1px solid blue";
+    sketchPad.appendChild(box)
     
-    
-    box.addEventListener("mouseover", function() {
-    box.style.backgroundColor = "orange"
-})
 }
+// add color on mouseover
+document.body.addEventListener("mouseover", function(e) {
+    if(e.target.className == "boxes") {
+        e.target.style.backgroundColor = "orange"
+    }
+})
+// add button
+newGrid.addEventListener("click", changeTheGrid)
+//make new grid 
+function changeTheGrid() {
+    sketchPad.innerHTML = "";
+    let userInput = prompt("enter a number of squares per side that is less than 100", "0")
+    let sideBoxes = parseInt(userInput)
+    sketchPad.style.gridTemplateColumns = `repeat(${sideBoxes}, 1fr)`
+    sketchPad.style.gridTemplateRows = `repeat(${sideBoxes}, 1fr)`
+    let grid = sideBoxes * sideBoxes;
 
-
-
+    for(let i = 1; i <= grid; i++) {
+        let box = document.createElement("div");
+        box.className = "boxes"
+        box.style.border = "1px solid blue";
+        sketchPad.appendChild(box)   
+    }
+}
